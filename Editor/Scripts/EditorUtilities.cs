944c5a79f9d8b355ea29f37faa8890fc0aff37e4
Tyler Wiebe <bibbi8448@gmail.com>
2022-07-24 14:51:26 -0500

    Update

---
+++ Editor/Scripts/EditorUtilities.cs
@@ -0,0 +1,56 @@
+﻿namespace TWIEditor.Sinespace
+{
+    using Newtonsoft.Json;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+    using UnityEditor;
+
+    public static class EditorUtilities
+    {
+        private const string LIBRARY_PACKAGE_CACHE = "Library/PackageCache/";
+
+        public static readonly IReadOnlyCollection<(string name, string path)> LocalDependencies = GetLocalDependencies();
+
+        public static string GetCallerFilePath([CallerFilePath] string callerFilePath = default) => callerFilePath;
+
+        public static string GetFullPath(string path)
+        {
+            if (path.StartsWith("%")) return Environment.ExpandEnvironmentVariables(path).Replace('\\', '/');
+            else path = path.Replace('\\', '/');
+
+            if (path.StartsWith(LIBRARY_PACKAGE_CACHE))
+            {
+                path = path.Substring(LIBRARY_PACKAGE_CACHE.Length);
+                string package = path.Substring(0, path.IndexOf('/'));
+                if (package.Contains('@')) package = package.Substring(0, package.IndexOf('@'));
+                return Path.GetFullPath("Packages/" + package + path.Substring(path.IndexOf('/'))).Replace('\\', '/');
+            }
+
+            var localDependency = LocalDependencies.FirstOrDefault(d => path.StartsWith(d.path));
+            if (string.IsNullOrWhiteSpace(localDependency.name)) return Path.GetFullPath(path).Replace('\\', '/');
+            else return Path.GetFullPath("Packages/" + localDependency.name + path.Substring(localDependency.path.Length)).Replace('\\', '/');
+        }
+
+        private static IReadOnlyCollection<(string name, string path)> GetLocalDependencies()
+        {
+            if (File.Exists("Packages/manifest.json"))
+            {
+                string json = File.ReadAllText("Packages/manifest.json");
+                Dictionary<string, Dictionary<string, string>> manifest = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, string>>>(json);
+                if (manifest.TryGetValue("dependencies", out Dictionary<string, string> dependencies))
+                {
+                    string prefix = "file:";
+                    List<(string, string)> localDependencies = new List<(string, string)>();
+                    foreach (KeyValuePair<string, string> dependency in dependencies)
+                        if (dependency.Value.StartsWith(prefix)) localDependencies.Add((dependency.Key, dependency.Value.Substring(prefix.Length)));
+                    return localDependencies;
+                }
+            }
+
+            return new List<(string, string)>();
+        }
+    }
+}
---
+++ Editor/Scripts/UpgradeSinespace.cs
@@ -0,0 +1,80 @@
+﻿namespace TWIEditor.Sinespace
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using Unity.EditorCoroutines.Editor;
+    using UnityEditor;
+    using UnityEngine;
+
+    using Stopwatch = System.Diagnostics.Stopwatch;
+
+    public static class UpgradeSinespace
+    {
+        private static EditorCoroutine coroutine;
+        private const long YIELD_RATE = 40;
+        private const string PROGRESS_TITLE = "Upgrading Sinespace... {0:N0}/{1:N0} ({2:P0})";
+        private const string PROGRESS_INFO = "Upgrading '{0}', please wait...";
+
+        [MenuItem("TWI/Sinespace/Upgrade Sinespace")]
+        public static void Upgrade()
+        {
+            if (coroutine == null) coroutine = EditorCoroutineUtility.StartCoroutineOwnerless(StartUpgrade());
+        }
+
+        private static IEnumerator StartUpgrade()
+        {
+            string path = EditorUtilities.GetCallerFilePath();
+            path = EditorUtilities.GetFullPath(path);
+            path = Path.GetDirectoryName(path);
+            path += "/../../Upgrades/";
+
+            Stopwatch stopwatch = new Stopwatch();
+            DirectoryInfo directory  = new DirectoryInfo(path);
+            IEnumerable<FileInfo> files = directory.EnumerateFiles("*.*", SearchOption.AllDirectories);
+            files = files.Where(f => f.Extension != ".meta");
+            stopwatch.Start();
+
+            float index = 0;
+            int count = files.Count();
+
+            foreach (var file in directory.GetFiles("*.*", SearchOption.AllDirectories))
+            {
+                index++;
+                path = file.FullName;
+                switch (file.Extension)
+                {
+                    case ".txt": path = Path.ChangeExtension(path, ".cs"); break;
+                    case ".json":
+                        break;
+                    default: throw new NotSupportedException("Extension '" + file.Extension + "' not supported");
+
+                }
+
+                if (stopwatch.ElapsedMilliseconds > YIELD_RATE)
+                {
+                    title = string.Format(PROGRESS_TITLE, index, count, index / count);
+                    info = string.Format(PROGRESS_INFO, Path.GetFileName(path));
+
+                    if (EditorUtility.DisplayCancelableProgressBar(title, info, index / count) && StopUpgrade()) yield break;
+
+                    yield return null;
+                    stopwatch.Reset();
+                }
+
+                path = "Assets/" + path.Substring(directory.FullName.Length);
+                path = path.Replace('\\', '/');
+                file.CopyTo(path, true);
+            }
+
+            AssetDatabase.Refresh();
+            coroutine = null;
+        }
+        private static bool StopUpgrade()
+        {
+            return false;
+        }
+    }
+}
